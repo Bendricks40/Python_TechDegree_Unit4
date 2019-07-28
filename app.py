35c599e0a5182f45c2b68d6de6c18bfa791f561e
Bendricks40 <bendricks40@gmail.com>
2019-07-28 13:40:49 -0400

    more work

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from collections import OrderedDict
 import os
 
 
+"""
+Helpful documentation around Peewee ORM and retrieving data:  http://docs.peewee-orm.com/en/latest/peewee/quickstart.html#retrieving-data
+"""
+
 db = SqliteDatabase('inventory.db')
 
 
@@ -19,11 +23,27 @@ class Product(Model):
         database = db
 
 
+def welcome():
+    print('\n')
+    print("**************************************  Welcome to Store Inventory!  *************************************")
+    print(" - This app lets you view a database full of store inventory items, and allows you to update as needed")
+    print(' - To begin, view the options below and enter the appropriate letter to make a selection (then press enter)')
+    print('**********************************************************************************************************')
+
+
+def initialize():
+    """create table and db"""
+    db.connect()
+    # print("Just connected to DB")
+    db.create_tables([Product], safe=True)
+    # print("just created tables")
+
+
 def open_and_clean_csv():
     with open('inventory.csv', newline='') as csvfile:
         artreader = csv.DictReader(csvfile, delimiter=',')
         rows = list(artreader)
-        for row in rows[1:]:
+        for row in rows[:]:
 
             # store product quantity as an int vs string
             row['product_quantity'] = int(row['product_quantity'])
@@ -51,18 +71,13 @@ def open_and_clean_csv():
             #print(product.product_price)
 
 
-def clear():
-    os.system('cls' if os.name == 'nt' else 'clear')
-    os.system('clear')
-
-
 def menu_loop():
     """Show the menu"""
     choice = None
 
     while choice != 'q':
         clear()
-        print("Enter 'q' to quit")
+        print("Choose an option below, or press 'q' to quit")
         for key, value in menu.items():
             print('{}) {}'.format(key, value.__doc__))
         choice = input('Action: ').lower().strip()
@@ -72,12 +87,9 @@ def menu_loop():
             menu[choice]()
 
 
-def initialize():
-    """create table and db"""
-    db.connect()
-    print("Just connected to DB")
-    db.create_tables([Product], safe=True)
-    print("just created tables")
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system('clear')
 
 
 def add_product():
@@ -87,29 +99,36 @@ def add_product():
 
 def view_product():
     """View the details of a specific product"""
-    print("you made it into the 'view product' Function!!!!!!!!")
-
+    choice = input("Enter the product ID you'd like to see details about: ")
+    print("ID:     NAME:                                   DATE UPDATED:        QUANTITY:   PRICE:")
+    for product in Product.select().where(Product.product_id == choice):
+        print_product(product)
+    print('\n')
 
 def view_every_product():
     """View EVERY product and their details"""
     print("\nHere are all the products in the database:")
-    # to get everything to line up nicely, I used the len of prior
-    # value to fit things into same starting position
-    print("ID:     NAME:                                   PRICE:  QUANTITY:")
+    print("ID:     NAME:                                   DATE UPDATED:        QUANTITY:   PRICE:")
     for product in Product.select():
-        print(str(product.product_id) +
-              (' '*(8-len(str(product.product_id)))) +
-              product.product_name +
-              (' '*(40-len(str(product.product_name)))) +
-              str(product.product_price) +
-              (' '*(8-len(str(product.product_price)))) +
-              str(product.product_quantity))
+        print_product(product)
     print('\n')
 
 
 def backup_products():
     """Creates a backup of entire database to a csv file"""
     print("you made it into the backup product function!!!!!!!")
+
+
+def print_product(product):
+    print(str(product.product_id) +
+          (' ' * (8 - len(str(product.product_id)))) +
+          product.product_name +
+          (' ' * (40 - len(str(product.product_name)))) +
+          str(product.timestamp)[:-9] +
+          (' ' * (30 - len(str(product.timestamp)))) +
+          str(product.product_quantity) +
+          (' ' * (12 - len(str(product.product_quantity)))) +
+          '$' + str(product.product_price / 100))
 
 
 menu = OrderedDict([
@@ -121,7 +140,7 @@ menu = OrderedDict([
 
 
 if __name__ == '__main__':
+    welcome()
     initialize()
     open_and_clean_csv()
     menu_loop()
-
