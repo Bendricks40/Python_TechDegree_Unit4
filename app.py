a050ffe053009fdf5a72fdad5217f87d909d82cf
Bendricks40 <bendricks40@gmail.com>
2019-07-29 22:36:41 -0400

    final changes Hi CHRIS

--- app.py
+++ app.py
@@ -7,8 +7,10 @@ import os
 
 
 """
-Helpful documentation around Peewee ORM and retrieving data:  http://docs.peewee-orm.com/en/latest/peewee/quickstart.html#retrieving-data
-Other links:
+Helpful documentation around Peewee ORM and retrieving data:  
+http://docs.peewee-orm.com/en/latest/peewee/quickstart.html#retrieving-data
+Other links: 
+https://stackoverflow.com/questions/41915140/delete-the-contents-of-a-file-before-writing-to-it-in-python/41915206
 """
 
 
@@ -64,21 +66,22 @@ def open_and_clean_csv():
 
             # now add the item to the database:
             if Product.select().where(Product.product_name.contains(row['product_name'])):
-                print("The product '{}' already exists. Checking if this is "
-                      "a newer version...".format(row['product_name']))
+                # print("The product '{}' already exists. Checking if this is "
+                # "a newer version...".format(row['product_name']))
                 for product in Product.select().where(Product.product_name.contains(row['product_name'])):
-                    print('The existing timestamp for this item is: {}, and the '
-                          'item we are trying to insert has a timestamp '
-                          'of {}'.format(product.timestamp, row['date_updated']))
+                    # print('The existing timestamp for this item is: {}, and the '
+                    # 'item we are trying to insert has a timestamp '
+                    # 'of {}'.format(product.timestamp, row['date_updated']))
                     if product.timestamp < row['date_updated']:
-                        print('Looks like we have a newer timestamp! Time to '
-                              'update the quantity/price/timestamp with this new info')
+                        # print('Looks like we have a newer timestamp! Time to '
+                        # 'update the quantity/price/timestamp with this new info')
                         product.timestamp = row['date_updated']
                         product.product_quantity = row['product_quantity']
                         product.product_price = row['product_price']
                         product.save()
                     else:
-                        print("this is NOT a newer timestamp, so nothing to do here.")
+                        pass
+                        # print("this is NOT a newer timestamp, so nothing to do here.")
             else:
                 Product.create(product_name=row['product_name'],
                                timestamp=row['date_updated'],
@@ -100,6 +103,8 @@ def menu_loop():
         if choice in menu:
             clear()
             menu[choice]()
+        else:
+            print("Oops, that wasn't a valid selection. Try again...\n")
 
 
 def clear():
@@ -110,21 +115,59 @@ def clear():
 def add_product():
     """Add a new product"""
     newProduct_name = input("Enter a name for the new product: ")
-    newProduct_quantity = input("Enter total available quantity for this product: ")
-    newProduct_price = input('Enter a price for the product in x.xx format (no dollar sign needed): ')
-    Product.create(product_name=newProduct_name,
-                   timestamp=datetime.now().strftime('%Y-%m-%d'),
-                   product_quantity=newProduct_quantity,
-                   product_price=newProduct_price)
+    validPrice = False
+    validQuantity = False
+
+    while not validQuantity:
+        try:
+            newProduct_quantity = int(input("Enter total available quantity for this product: "))
+            validQuantity = True
+        except ValueError:
+            print("Not a valid quantity, try again!")
+
+    while not validPrice:
+        try:
+            newProduct_price = int(input('Enter a price for the product in pennies--i.e. '
+                                         '$3.44 would be entered as 344 (no dollar sign needed): '))
+            validPrice = True
+        except ValueError:
+            print("Not a valid price, try again!")
+
+    if validPrice and validQuantity:
+        if Product.select().where(Product.product_name == newProduct_name):
+            for product in Product.select().where(Product.product_name == newProduct_name):
+                product.timestamp = datetime.now().strftime('%Y-%m-%d')
+                product.product_quantity = newProduct_quantity
+                product.product_price = newProduct_price
+                product.save()
+                print("\nThis product already existed, but we took your newer data "
+                      "and saved it so that now '{}' has been updated successfully!\n\n".format(newProduct_name))
+        else:
+            Product.create(product_name=newProduct_name,
+                           timestamp=datetime.now().strftime('%Y-%m-%d'),
+                           product_quantity=int(newProduct_quantity),
+                           product_price=int(newProduct_price))
+            print("\n'{}' has been successfully added to the database!\n\n".format(newProduct_name))
 
 
 def view_product():
     """View the details of a specific product by entering it's ID"""
-    choice = input("Enter the product ID you'd like to see details about: ")
-    print(header)
-    for product in Product.select().where(Product.product_id == choice):
-        print_product(product)
-    print('\n')
+    exists = False
+    while not exists:
+
+        try:
+            choice = int(input("Enter the product ID you'd like to see details about: "))
+            for product in Product.select().where(Product.product_id == choice):
+                exists = True
+                print(header)
+                print_product(product)
+                break
+            else:
+                print("That ID doesn't exist...try again!")
+            print('\n')
+
+        except ValueError:
+            print("Oops, that wasn't a valid number. Try again with a numeric ID")
 
 
 def view_every_product():
@@ -138,7 +181,26 @@ def view_every_product():
 
 def backup_products():
     """Creates a backup of entire database to a csv file"""
-    print("you made it into the backup product function!!!!!!!")
+    with open('backup.csv', 'a') as csvfile:
+        # Clear out existing file contents, if any
+        csvfile.seek(0)
+        csvfile.truncate()
+        # Now write the new stuff into the file by first creating the header names:
+        fieldnames = ['product_id', 'product_name', 'product_timestamp', 'product_quantity', 'product_price']
+        # Create the actual CSV writer object:
+        productWriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        # Throw a good ol' header in there:
+        productWriter.writeheader()
+        # Now walk through each product and write it as a line into the file:
+        for product in Product.select():
+            productWriter.writerow({'product_id': product.product_id,
+                                    'product_name': product.product_name,
+                                    'product_timestamp': product.timestamp,
+                                    'product_quantity': product.product_quantity,
+                                    'product_price': product.product_price})
+    # Close out the file:
+    csvfile.close()
+    print("\nBackup has been created entitled 'backup.csv'\n\n")
 
 
 def print_product(product):
